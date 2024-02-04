6dc26e7107a4e14264e865bf53db73ed9fc473ff
anilanandhu <48132983+anilanandhu@users.noreply.github.com>
2024-02-03 20:11:03 -0600

    Update wifi (1).ino

--- final code/wifi (1).ino
+++ final code/wifi (1).ino
@@ -9,7 +9,7 @@ abcd
 
 #define FIREBASE_AUTH "Ypf2lgSXcxhrb5dqz6K75ebJtXSz5Zenz3ggFFux"
 
-#define WIFI_SSID "Mubeen Ahmed"
+#define WIFI_SSID "Mubeen Ahmed"frewqas
 
 #define WIFI_PASSWORD "mubeen777"
 njjjj
